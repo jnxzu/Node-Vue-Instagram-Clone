f865835d9650a5c697c264024f36fda85f1a01b9
J <janbielowka@gmail.com>
2020-10-26 12:32:39 +0100

    app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <router-view />
+    <Nav />
+    <div class="main">
+      <router-view />
+    </div>
   </div>
 </template>
 
-<style>
+<script>
+import Nav from './components/Nav.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Nav,
+  },
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap');
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: 'Open Sans', sans-serif;
+  --bg: #fafafa;
+  --accent: rgb(247, 142, 88);
+  --border: #dbdbdb;
+}
+
+#app {
+  width: 100vw;
+}
+
+.main {
+  background: var(--bg);
+  margin-top: 55px;
 }
 </style>
