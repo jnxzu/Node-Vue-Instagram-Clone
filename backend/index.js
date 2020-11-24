68d21f459fc2afd5362ccb3f6a8e52f72cf91b98
Marta Rybarczyk <martarybarczyk98@gmail.com>
2020-11-24 14:36:53 +0100

    Firebase added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
-
+const {Storage} = require('@google-cloud/storage');
 const app = express();
 const cors = require('cors');
 const session = require('express-session');
@@ -9,7 +9,8 @@ const errorHandler = require('error-handler');
 const logger = require('morgan');
 const axios = require('axios');
 const MongoStore = require('connect-mongo')(session);
-
+const firebase = require('firebase');
+const fs = require('fs');
 const path = require('path');
 const { Console } = require('console');
 
@@ -23,7 +24,8 @@ const server = require('./https')(app);
 const port = process.env.PORT;
 // const Post = require('./models/Post');
 const users = require('./routes/UserRoutes');
-// const AuctionRoutes = require('./routes/PostRoutes');
+//const posts = require('./routes/PostRoutes');
+const { rootCertificates } = require('tls');
 // const messageRoutes = require('./routes/MessageRoutes');
 // const postServices = require('./services/PostServices');
 // const roomRoutes = require('./routes/RoomRoutes');
@@ -68,6 +70,7 @@ app.use(express.static(path.join(__dirname, 'client/dist')));
 
 // Routing
 app.use('/UserRoutes', users);
+//app.use('/PostRoutes', posts);
 // app.use('/api/auctions', AuctionRoutes);
 // app.use('/api/messages', messageRoutes);
 // app.use('/api/rooms', roomRoutes);
@@ -82,6 +85,34 @@ const axiosConfig = {
 };
 
 axios.config = axiosConfig;
+
+var firebaseConfig = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  databaseURL: process.env.FB_DATABASE_URL,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MSG_SENDER_ID,
+  appId: process.env.FB_APP_ID
+};
+
+// https://cloud.google.com/appengine/docs/standard/nodejs/using-cloud-storage
+firebase.initializeApp(firebaseConfig);
+const storage = new Storage({
+  keyFilename: "fb-key-file.json",
+});
+let bucketName = process.env.FB_STORAGE_BUCKET;
+const downloadFile = async(filename) => {
+  await storage.bucket(bucketName).file(filename).createReadStream().on('error', function(err) {})
+  .on('response', function(response) {
+    // Server connected and responded with the specified status and headers.
+   })
+  .on('end', function() {
+    // The file is fully downloaded.
+  })
+  .pipe(fs.createWriteStream(filename));;
+}
+downloadFile('maskonur2.png');
 
 server.listen(port, () => {
   console.log(`Serwer działa pod adresem: https://localhost:${port}`);
