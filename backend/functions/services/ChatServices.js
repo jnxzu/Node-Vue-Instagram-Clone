b91ed8acfeb387d5fdb9e7f3429435ac65de33d4
Dominika Cesarz <dominikacesarz@yahoo.com>
2020-12-27 16:54:23 +0100

    small delete

--- backend/functions/services/ChatServices.js
+++ backend/functions/services/ChatServices.js
@@ -3,24 +3,6 @@ const moment = require('moment');
 const Chat = require('../models/Chat');
 const User = require('../models/User');
 
-// const {Message} = Chat
-
-// const findChat = (req, res) => {
-//   const filtr = {
-//     $in: [
-//       { users: req.body.user1},
-//       { users: req.body.user2},
-//     ],
-//   };
-//   Chat.find(filtr, function (err, doc) {
-//     if (err) {
-//       res.json(err);
-//     } else {
-//       res.json(doc);
-//     }
-//   });
-// };
-
 module.exports.newMessage = (req, res) => {
       const msg = {
         author: req.user.id,
